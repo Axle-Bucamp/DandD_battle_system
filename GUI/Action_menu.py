d383c17c5c1436cf48913f4447155e7ad5ca1c10
Axle-Bucamp <axle.bucamp@gmail.com>
2022-08-08 18:02:32 +0200

    throw random dice

--- GUI/Action_menu.py
+++ GUI/Action_menu.py
@@ -10,6 +10,8 @@ from GUI import popup_battle_draw
 from GUI import popup_load_json
 from BattleSystem.BattleField import battle_field
 from BattleSystem.Ability_manager import Ability_manager
+from kivy.uix.popup import Popup
+from BattleSystem.Dice import Dice
 import json
 
 class Action_menu(Accordion):
@@ -23,6 +25,8 @@ class Action_menu(Accordion):
         self.add_widget(ability_management)
         file_management = AccordionItem(title="File")
         self.add_widget(file_management)
+        dice_management = AccordionItem(title="Dice")
+        self.add_widget(dice_management)
 
         # drawing battle pannel :
         battle_options = GridLayout(cols=1)
@@ -109,12 +113,67 @@ class Action_menu(Accordion):
 
         file_management.add_widget(file_options)
 
+        # Dice management
+        dice_options = GridLayout(cols=1)
+        self.throw_4dice = Button(text="dice 4", size_hint_y=None, height=44)
+        self.throw_4dice.bind(on_press=lambda x: self.popup_dice(4))
+        dice_options.add_widget(self.throw_4dice)
+
+        self.throw_6dice = Button(text="dice 6", size_hint_y=None, height=44)
+        self.throw_6dice.bind(on_press=lambda x: self.popup_dice(6))
+        dice_options.add_widget(self.throw_6dice)
+
+        self.throw_8dice = Button(text="dice 8", size_hint_y=None, height=44)
+        self.throw_8dice.bind(on_press=lambda x: self.popup_dice(8))
+        dice_options.add_widget(self.throw_8dice)
+
+        self.throw_12dice = Button(text="dice 12", size_hint_y=None, height=44)
+        self.throw_12dice.bind(on_press=lambda x: self.popup_dice(12))
+        dice_options.add_widget(self.throw_12dice)
+
+        self.throw_20dice = Button(text="dice 20", size_hint_y=None, height=44)
+        self.throw_20dice.bind(on_press=lambda x: self.popup_dice(20))
+        dice_options.add_widget(self.throw_20dice)
+
+        self.throw_100dice = Button(text="dice 100", size_hint_y=None, height=44)
+        self.throw_100dice.bind(on_press=lambda x: self.popup_dice(100))
+        dice_options.add_widget(self.throw_100dice)
+
+        dice_management.add_widget(dice_options)
+
     def remove_entity(self, select1, select2):
         if select1 is not None:
             battle_field.entities.remove(select1)
             Main_window.refresh()
 
         self.popup_remove_entity.dismiss()
+    @staticmethod
+    def popup_dice(res):
+        pop = Popup(title="Dice " + str(res), size_hint=(None, None), size=(600, 600))
+        layout = GridLayout(cols=1)
+        pop.add_widget(layout)
+
+        if res < 100:
+            if res < 20:
+                if res < 12:
+                    if res < 8:
+                        if res < 6:
+                            dice = Dice.dice4()
+                        else:
+                            dice = Dice.dice6()
+                    else:
+                        dice = Dice.dice8()
+                else:
+                    dice = Dice.dice12()
+            else:
+                dice = Dice.dice20()
+        else:
+            dice = Dice.dice100()
+
+        btn = Button(text=str(dice) + "/" + str(res))
+        btn.bind(on_release=pop.dismiss)
+        layout.add_widget(btn)
+        pop.open()
 
     def remove_ability(self, select1, select2):
         Ability_manager.abilities.remove(select1)

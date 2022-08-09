9b609e6b4d68e3fefd1d4dd98189686c697483ac
Axle-Bucamp <axle.bucamp@gmail.com>
2022-08-09 10:28:37 +0200

    healing effect and dot update

--- BattleSystem/Entity.py
+++ BattleSystem/Entity.py
@@ -155,8 +155,10 @@ class Entity:
 
             dot.turn_left -= 1
             if dot.turn_left < 0:
+                print(str(dot.name)+ "has been remove")
                 self.dot_list.remove(dot)
 
+        print(str(dam) + " self dot value this turn")
         self.hit_point -= dam
 
     def rest(self):

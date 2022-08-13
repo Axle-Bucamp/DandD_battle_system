0ec4a980d28d0bb81e20392592f47a4fcdfe7422
Axle-Bucamp <axle.bucamp@gmail.com>
2022-08-13 19:43:11 +0200

    starting gear and inventory system

--- BattleSystem/__init__.py
+++ BattleSystem/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["Ability", "Ability_manager", "BattleField",
            "Buff_effect", "Dice", "Dot_effect", "Effect",
-           "Entity", "Bestiary"]
+           "Entity", "Bestiary", "Gear", "Item_manager"]
 
